f0e70ced7bf58c8fe7319fd7072689d10169dcc3
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2020-01-20 18:37:50 +0200

    Export LTI keys for A+

--- rootfs/srv/jutut-init.sh
+++ rootfs/srv/jutut-init.sh
@@ -7,5 +7,11 @@ else
     setuidgid $USER python3 manage.py list_lti_keys -k testjutut 2>/dev/null
 fi
 
+# Export LTI keti for A+
+mkdir -p /data/aplus/lti-services-in/
+setuidgid $USER python3 \
+    manage.py export_for_aplus -k testjutut -l Jutut -i send -b http://$(hostname -i):8082 \
+    > /data/aplus/lti-services-in/jutut.json
+
 # Start background services/tasks
 start_services jutut-celery-worker jutut-celery-beat
